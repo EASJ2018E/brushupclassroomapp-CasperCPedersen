3106e91f6450231a1a8bf8bf85e2b0c3cccc04e4
Casper Chaithon Pedersen <casp6665@edu.easj.dk>
2018-09-09 23:03:01 +0200

    Main done

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,15 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            KlasseRum TestKlasse = new KlasseRum();
+            TestKlasse.KlasseNavn = "3B";
+            DateTime semesterstart = new DateTime(2018,9,4);
+            TestKlasse.SemesterStart = semesterstart;
+            Studerende Casper = new Studerende("Casper",6,29);
+            TestKlasse.KlasseList.Add(Casper);
+            Studerende Søren = new Studerende("Søren",1,1);
+            TestKlasse.KlasseList.Add(Søren);
+
         }
     }
 }
