fd35b788830663440528f6eb50d41d9b2f2871f6
Casper Chaithon Pedersen <casp6665@edu.easj.dk>
2018-09-09 22:50:31 +0200

    KlasseRum done

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        public string KlasseNavn { get; set; }
+
+        public List<Studerende> KlasseList { get; set; }
+
+        public DateTime SemesterStart { get; set; }
+
+        public KlasseRum()
+        {
+            
+        }
+
+    }
+}
