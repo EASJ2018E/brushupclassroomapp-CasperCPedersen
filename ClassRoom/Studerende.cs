cc48880667a9d2ce44f9de4eb6d8396c172016a5
Casper Chaithon Pedersen <casp6665@edu.easj.dk>
2018-09-09 22:43:07 +0200

    Studerende done

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+        public string Name { get; set; }
+
+        public int FødeslsMåned { get; set; }
+
+        public int FødeslsDag { get; set; }
+
+        public Studerende(string name, int fødeslsMåned, int fødeslsDag)
+        {
+            Name = name;
+            FødeslsMåned = fødeslsMåned;
+            FødeslsDag = fødeslsDag;
+        }
+    }
+}
